80ac4d9054848d585e06d3bfdd8d33b92548af23
LEE YOO JOON <87304234+qewr1234@users.noreply.github.com>
2026-08-12 20:41:37 +0900

    [Passed] 입력받은 값과 합 출력 (2ms, 0MB)

---
+++ trail0/입력받은 값과 합 출력/output-of-value-and-sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int A, B;
+
+    cin >> A >> B;
+
+    cout << A << " " << B << " " << A+B << endl;
+    return 0;
+}
