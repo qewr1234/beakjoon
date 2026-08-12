db7c055176ab001e5d9b56c1ccd8c00c0b3e2b5c
LEE YOO JOON <87304234+qewr1234@users.noreply.github.com>
2026-08-12 20:21:19 +0900

    [Passed] 세 실수의 반올림 (2ms, 0MB)

---
+++ trail0/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float a, b, c = 0;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    cout << fixed;
+    cout.precision(3);
+    cout << a << "\n" << b << "\n" << c << endl;
+    return 0;
+}
