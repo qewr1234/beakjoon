0ac571d70f05cb86175a2daa3b138b91a78d95ed
LEE YOO JOON <87304234+qewr1234@users.noreply.github.com>
2026-08-12 19:01:38 +0900

    [Passed] 실수 받아 그대로 출력 (2ms, 0MB)

---
+++ trail0/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a = 0;
+
+    cin >> a;
+
+    cout << fixed;
+    cout.precision(2);
+    cout << a << endl;
+    return 0;
+}
