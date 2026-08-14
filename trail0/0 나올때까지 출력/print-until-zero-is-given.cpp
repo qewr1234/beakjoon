975d0da882ca514a9f11bd44f004a5739b49a593
LEE YOO JOON <87304234+qewr1234@users.noreply.github.com>
2026-08-14 21:55:27 +0900

    [Passed] 0 나올때까지 출력 (1ms, 0MB)

---
+++ trail0/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 0;
+    
+    while(true){
+        cin >> n;
+        if(n == 0){
+            break;
+        }
+        cout << n << endl;
+    }
+    return 0;
+}
