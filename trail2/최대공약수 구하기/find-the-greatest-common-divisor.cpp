70ddf2892df947a16d068bfcb8a31ca3f8916663
LEE YOO JOON <87304234+qewr1234@users.noreply.github.com>
2026-08-16 21:20:07 +0900

    [Passed] 최대공약수 구하기 (2ms, 0MB)

--- trail2/최대공약수 구하기/find-the-greatest-common-divisor.cpp
+++ trail2/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -2,14 +2,7 @@
 using namespace std;
 
 int gcd(int n, int m){
-    int array[m];
     int result = 1;
-    if(n > m){
-        int temp = 0;
-        temp = n;
-        n = m;
-        m = temp;
-    }
     for(int i = 1; i <= m; i++){
         if(n % i == 0 && m % i == 0){
             result = i;
