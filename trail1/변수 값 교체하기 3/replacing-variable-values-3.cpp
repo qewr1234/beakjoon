5b6f61003cb49e6326aede1e203e7531476edf85
LEE YOO JOON <87304234+qewr1234@users.noreply.github.com>
2026-08-16 20:00:26 +0900

    [Passed] 변수 값 교체하기 3 (2ms, 0MB)

---
+++ trail1/변수 값 교체하기 3/replacing-variable-values-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 3, b = 5;
+    int temp = 0;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << a << "\n" << b;
+    return 0;
+}
