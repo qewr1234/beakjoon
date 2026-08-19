a79767abce662e4e6046073119c5706254387486
LEE YOO JOON <87304234+qewr1234@users.noreply.github.com>
2026-08-19 23:24:51 +0900

    [Passed] 함수를 이용한 합과 소수 판별 (2ms, 0MB)

--- trail2/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.cpp
+++ trail2/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 bool CheckPrimeNum(int n){
-    for(int i = 2; i < n; i++){
+    if(n < 2){
+        return false;
+    }
+    for(int i = 2; i * i <= n; i++){
         if(n % i == 0){
             return false;
         }
